7159b13959d310e52c61312cf9f7b8003a214aeb
anikatasnin <jarintasnin09@gmail.com>
2025-01-27 13:20:43 +0600

    Added Taxi

--- Taxi/taxi.cpp
+++ Taxi/taxi.cpp
@@ -5,9 +5,13 @@ int main(){
     long long n;
     cin >> n;
 
-    int arr[n];
+    int arr[n], sum = 0;
     for(int i = 0; i < n; i++){
         cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
     }
 
     return 0;
