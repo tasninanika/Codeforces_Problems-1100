d23baac0578110464ffa7c570ae5dd3c8a490c8e
anikatasnin <jarintasnin09@gmail.com>
2025-03-12 09:56:23 +0600

    added Interesting drink

--- Interesting drink/interestingdrink.cpp
+++ Interesting drink/interestingdrink.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
@@ -9,6 +9,8 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> x[i];
     }
+
+    sort(x, x+n);
 
     int q;
     cin >> q;
