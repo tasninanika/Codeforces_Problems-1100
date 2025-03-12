a032ed054a0883021bb5b1482435c2b48bdf1839
anikatasnin <jarintasnin09@gmail.com>
2025-03-12 09:57:03 +0600

    added Interesting drink

--- Interesting drink/interestingdrink.cpp
+++ Interesting drink/interestingdrink.cpp
@@ -15,20 +15,7 @@ int main(){
     int q;
     cin >> q;
 
-    int m[q];
-    for(int i = 0; i < q; i++){
-        cin >> m[i];
-    }
 
-    for(int i = 0; i < q; i++){
-        int c = 0;
-        for(int j = 0; j < n; j++){
-            if(m[i] >= x[j]){
-                c++;
-            }
-        }
-        cout << c << endl;
-    }
 
     return 0;
 }
