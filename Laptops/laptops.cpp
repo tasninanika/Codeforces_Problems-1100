403f12dcbca9fc0d565bcb4f2bc947d30673d35a
anikatasnin <jarintasnin09@gmail.com>
2025-04-30 08:58:45 +0600

    added Laptops

--- Laptops/laptops.cpp
+++ Laptops/laptops.cpp
@@ -14,6 +14,7 @@ int main(){
         for(int j = i + 1; j < n; j++){
             if(price[i] > price[j]){
                 swap(price[i], price[j]);
+                swap(quality[i], quality[j]);
             }
         }
     }
