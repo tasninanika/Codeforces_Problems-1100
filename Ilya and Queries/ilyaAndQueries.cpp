ad771f16436e55c7a4f639534bd119f64ee93feb
anikatasnin <jarintasnin09@gmail.com>
2025-05-05 10:37:36 +0600

    added Ilya and Queries

--- Ilya and Queries/ilyaAndQueries.cpp
+++ Ilya and Queries/ilyaAndQueries.cpp
@@ -8,8 +8,8 @@ int main(){
     string s;
     cin>>s>>m;
 
-    for(int i=1;i<s.size();i++){
-        if(s[i]==s[i-1])
+    for(int i = 1; i < s.size(); i++){
+        if(s[i] == s[i-1])
             c++;
         dp[i] = c;
     }
