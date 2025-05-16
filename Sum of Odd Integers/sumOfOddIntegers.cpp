79d042cd8f61573239079533ddbcbbaf372b3b6a
anikatasnin <jarintasnin09@gmail.com>
2025-05-16 12:28:23 +0600

    added Sum of Odd Integers

--- Sum of Odd Integers/sumOfOddIntegers.cpp
+++ Sum of Odd Integers/sumOfOddIntegers.cpp
@@ -11,6 +11,9 @@ int main(){
         if(k * k > n){
             cout << "NO" << endl;
         }
+        else if((n % 2 == 1 && k % 2 == 1) || (n % 2 == 0 && k % 2 == 0)){
+
+        }
 
     }
 
