051c319ddcb1599db5fcfef9f6afa50df007b605
anikatasnin <jarintasnin09@gmail.com>
2025-05-18 10:54:48 +0600

    added Two Teams Composing

--- Two Teams Composing/twoTeamsComposing.cpp
+++ Two Teams Composing/twoTeamsComposing.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 
-int main() {
+int main(){
     int t;
     cin >> t;
 
 
-    while (t--) {
+    while (t--){
         int n;
         cin >> n;
 
         map<int, int> freq;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             int x;
             cin >> x;
             freq[x]++;
         }
 
         int max_freq = 0;
-        for (auto [num, count] : freq) {
+        for (auto [num, count] : freq){
             if (count > max_freq)
                 max_freq = count;
         }
