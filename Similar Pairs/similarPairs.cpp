f36d95a2e27ba719286a9a327666b222e1b8f37c
anikatasnin <jarintasnin09@gmail.com>
2025-05-24 18:52:30 +0600

    added Similar Pairs

--- Similar Pairs/similarPairs.cpp
+++ Similar Pairs/similarPairs.cpp
@@ -5,6 +5,18 @@ int main(){
     int t,n,a[101];
     cin >> t;
 
+     while(t--){
+        int even = 0, odd = 0, dif = 0, diff = 0;
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            if(a[i] % 2 == 0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
 
     return 0;
 }
