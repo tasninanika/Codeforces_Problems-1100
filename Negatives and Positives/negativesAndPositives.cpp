66b96a213d80b2723118cacfb772e77895640599
anikatasnin <jarintasnin09@gmail.com>
2025-06-02 12:07:52 +0600

    added Negatives and Positives

--- Negatives and Positives/negativesAndPositives.cpp
+++ Negatives and Positives/negativesAndPositives.cpp
@@ -33,7 +33,7 @@ int main(){
             int x = arr[0];
             for(int i = 1; i < n; i++){
                 if(arr[i] < x){
-
+                    x = arr[i];
                 }
             }
         }
