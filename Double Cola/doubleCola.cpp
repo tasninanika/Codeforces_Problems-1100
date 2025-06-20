53b7548824f76986742c179cae87bc676485f13a
anikatasnin <jarintasnin09@gmail.com>
2025-06-20 12:07:35 +0600

    added Double Cola

--- Double Cola/doubleCola.cpp
+++ Double Cola/doubleCola.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin >> n;
+
+    int r = 1;
+    while(r * 5 < n)
+    {
+        n -= r * 5;
+        r *= 2;
+    }
+
+    string names[] = {"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+    cout << names[(n - 1) / r] << endl;
+
+    return 0;
+}
