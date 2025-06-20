9038bb3a78d53903440d1ca984307062f491ef4e
anikatasnin <jarintasnin09@gmail.com>
2025-06-20 11:52:15 +0600

    added Shovels and Swords

--- Shovels and Swords/shovelsAndSwords.cpp
+++ Shovels and Swords/shovelsAndSwords.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
+    int t;
+    long long a, b;
+    cin >> t;
 
+    while(t--){
+        cin >> a >> b;
+        if(a > b){
+            swap(a,b);
+        }
+        if(a*2 <= b){
+            cout << a << endl;
+        }
+        else{
+            cout << (a + b) / 3 << endl;
+        }
+    }
+
+    return 0;
 
 }
