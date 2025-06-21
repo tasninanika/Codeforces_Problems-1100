27b149a6c896ef3db7838aa776a9e782d207dfcb
anikatasnin <jarintasnin09@gmail.com>
2025-06-21 19:25:42 +0600

    added Eating Candies

--- Eating Candies/eatingCandies.cpp
+++ Eating Candies/eatingCandies.cpp
@@ -1,1 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        int i = -1, j = n;
+        int ans = 0;
+        int cnt = 0;
+        long long sa = 0, sb = 0;
+
+        while(i < j)
+        {
+            if(sa == sb)
+            {
+                ans = cnt;
+                sa = arr[++i];
+                cnt = 1;
+                sb = 0;
+            }
+            else if(sa < sb)
+            {
+                sa += arr[++i];
+                cnt++;
+            }
+            else
+            {
+                sb += arr[--j];
+                cnt++;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
 
