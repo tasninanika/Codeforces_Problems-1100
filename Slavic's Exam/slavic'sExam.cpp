4d202e55c10eb6c5a3ce9cc0e3a384b8f4538b1f
anikatasnin <jarintasnin09@gmail.com>
2025-06-26 18:15:13 +0600

    added Slavic's Exam

--- Slavic's Exam/slavic'sExam.cpp
+++ Slavic's Exam/slavic'sExam.cpp
@@ -12,7 +12,7 @@ int main(){
         int j = 0;
 
         for(int i = 0; i < s.size(); i++){
-            if(s[i] = t[j]){
+            if(s[i] == t[j]){
                 j++;
             }
             else if(s[i] == '?'){
