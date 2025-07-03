dc8735b98882f3c51721998fb330b4ce63d78031
anikatasnin <jarintasnin09@gmail.com>
2025-07-03 18:20:02 +0600

    added Parallelepiped

--- Parallelepiped/parallelepiped.cpp
+++ Parallelepiped/parallelepiped.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x, y, z;
+	cin >> x >> y >> z;
+
+	int a = sqrt((x * y) / z);
+	int b = sqrt((x * z) / y);
+	int c = sqrt((y * z) / x);
+
+	int val = (a + b + c) * 4;
+
+	cout << val;
+
+	return 0;
+}
 
