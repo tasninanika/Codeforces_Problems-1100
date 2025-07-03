d7c2a45e8975d600fedaa8f278d9c194c9256d85
anikatasnin <jarintasnin09@gmail.com>
2025-07-03 17:55:09 +0600

    added Yet Another Palindrome Problem

--- Yet Another Palindrome Problem/yetAnotherPalindromeProblem.cpp
+++ Yet Another Palindrome Problem/yetAnotherPalindromeProblem.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        int a[5001];
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        int found = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 2; j < n; j++){
+                if(a[i] == a[j]){
+                    found = 1;
+                    break;
+                }
+            }
+            if(found == 1){
+                break;
+            }
+        }
+
+        if(found == 1){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
 
