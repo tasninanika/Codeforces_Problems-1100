f5cd92a6d6878b026ed66e7e04195e9a808576fd
anikatasnin <jarintasnin09@gmail.com>
2025-07-14 23:14:07 +0600

    added Card Constructions

--- Card Constructions/cardConstructions.cpp
+++ Card Constructions/cardConstructions.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        long long n;
+        cin >> n;
+
+        long long c = 0;
+
+        while(n > 2){
+            long long t = 5;
+            long long now = 2;
+
+            while(n >= t + now){
+                now = t + now;
+                t += 3;
+            }
+
+            n -= now;
+            c++;
+        }
+
+        if(n >= 2){
+            c++;
+        }
+
+        cout << c << endl;
+    }
+
+    return 0;
+}
 
