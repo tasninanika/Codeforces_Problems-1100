288b7418736bda9430b3b86b4c2e7df7f2a609a8
anikatasnin <jarintasnin09@gmail.com>
2025-07-19 23:17:09 +0600

    added Double Strings

--- Double Strings/doubleStrings.cpp
+++ Double Strings/doubleStrings.cpp
@@ -14,7 +14,7 @@ int main(){
 
         for(int i = 0; i < n; i++){
             cin >> words[i];
-            mp[words[i] = 1;
+            mp[words[i]] = 1;
         }
 
         for(int i = 0; i < n; i++){
