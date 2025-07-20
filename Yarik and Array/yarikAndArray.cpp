aa69d2d2abec9bec74fc3996dadd5128023ca5b2
anikatasnin <jarintasnin09@gmail.com>
2025-07-21 00:06:50 +0600

    added Yarik and Array

--- Yarik and Array/yarikAndArray.cpp
+++ Yarik and Array/yarikAndArray.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        int prev = 0, cs = 0, mxs = -1007;
+        for(int i = 0; i < n; i++){
+            int x;
+            cin >> x;
+
+            if((prev + x) % 2 == 0 || cs < 0){
+                cs = 0;
+            }
+            cs += x;
+            mxs = max(mxs, cs);
+            prev = x % 2;
+        }
+
+        cout << mxs << endl;
+    }
+
+    return 0;
+}
 
