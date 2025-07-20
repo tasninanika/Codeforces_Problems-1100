6a6baba0dd156f01ae8606dd854cdf2a455b1ffc
anikatasnin <jarintasnin09@gmail.com>
2025-07-20 23:50:55 +0600

    added Yet Another Card Deck

--- Yet Another Card Deck/yetAnotherCardDeck.cpp
+++ Yet Another Card Deck/yetAnotherCardDeck.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> pos(51, 1e9);
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        pos[x] = min(pos[x], i + 1);
+    }
+
+    while(q--) {
+        int query;
+        cin >> query;
+        int temp = pos[query];
+        cout << temp << " ";
+        pos[query] = 1;
+
+        for (int j = 1; j <= 50; j++) {
+            if (j != query && pos[j] < temp) {
+                pos[j]++;
+            }
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
 
