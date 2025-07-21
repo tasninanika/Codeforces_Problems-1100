e8d2686cb51de05c515cd53387cb65d98c30c7c0
anikatasnin <jarintasnin09@gmail.com>
2025-07-21 22:55:25 +0600

    added Paint the Array

--- Paint the Array/paintTheArray.cpp
+++ Paint the Array/paintTheArray.cpp
@@ -1,1 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        long long ans = 0;
+
+        {
+            bool ok = true;
+            int prevColor = (a[0] % a[0] == 0);
+            for (int i = 1; i < n; i++) {
+                int cur = (a[i] % a[0] == 0);
+                if (cur == prevColor) { ok = false; break; }
+                prevColor = cur;
+            }
+            if (ok) ans = a[0];
+        }
+
+        if (ans == 0) {
+            bool ok = true;
+            int prevColor = (a[0] % a[1] == 0);
+            for (int i = 1; i < n; i++) {
+                int cur = (a[i] % a[1] == 0);
+                if (cur == prevColor) { ok = false; break; }
+                prevColor = cur;
+            }
+            if (ok) ans = a[1];
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
 
