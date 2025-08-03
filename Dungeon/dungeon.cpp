a391018feed4175e17a6ebc0fde4e286704a2c86
anikatasnin <jarintasnin09@gmail.com>
2025-08-03 16:42:14 +0600

    added Dungeon

--- Dungeon/dungeon.cpp
+++ Dungeon/dungeon.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+#define ll long long int
 
+int main(){
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; ++i){
+        ll a,b,c;
+        cin >> a >> b >> c;
+
+        int x = (a + b + c) / 9;
+
+        if(x<=min(a,min(b,c)) && (a+b+c)%9==0){
+            cout <<"YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
