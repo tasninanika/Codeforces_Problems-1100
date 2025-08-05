eb67d30c68f5de595ad4837b5c64bc46fb7dd407
anikatasnin <jarintasnin09@gmail.com>
2025-08-05 23:06:53 +0600

    added 250 Thousand Tons of TNT

--- 250 Thousand Tons of TNT/250ThousandTonsOfTNT.cpp
+++ 250 Thousand Tons of TNT/250ThousandTonsOfTNT.cpp
@@ -1,1 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+
+        vector<int> divisors;
+        for (int i = 1; i * i <= n; ++i) {
+            if (n % i == 0) {
+                divisors.push_back(i);
+                if (i != n / i) {
+                    divisors.push_back(n / i);
+                }
+            }
+        }
+
+        sort(divisors.begin(), divisors.end());
+
+        long long maxDifference = 0;
+
+        for (int size : divisors) {
+            long long maxSum = LLONG_MIN;
+            long long minSum = LLONG_MAX;
+            long long currentSum = 0;
+            int count = 0;
+
+            for (int i = 0; i < n; ++i) {
+                currentSum += a[i];
+                count++;
+
+                if (count == size) {
+                    maxSum = max(maxSum, currentSum);
+                    minSum = min(minSum, currentSum);
+                    currentSum = 0;
+                    count = 0;
+                }
+            }
+
+            maxDifference = max(maxDifference, maxSum - minSum);
+        }
+
+        cout << maxDifference << endl;
+    }
+
+    return 0;
+}
 
