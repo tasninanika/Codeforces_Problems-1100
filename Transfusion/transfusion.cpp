f7681daf4639f576eba5682147db49e1079813e1
anikatasnin <jarintasnin09@gmail.com>
2025-09-04 19:58:40 +0600

    added Transfusion

--- Transfusion/transfusion.cpp
+++ Transfusion/transfusion.cpp
@@ -1,1 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        bool ok = true;
+        for(int i = 1; i < n-1; i++){
+            if(a[i] != a[1]){
+                ok = false;
+                break;
+            }
+        }
+
+        if(ok){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
 
