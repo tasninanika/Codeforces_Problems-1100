4de6ca0e103520aff412dbffde3daa07a34701f2
anikatasnin <jarintasnin09@gmail.com>
2025-09-04 19:51:50 +0600

    added Sequential Nim

--- Sequential Nim/sequentialNim.cpp
+++ Sequential Nim/sequentialNim.cpp
@@ -5,11 +5,14 @@ int main(){
     int t;
     cin >> t;
 
-    while (t--) {
+    while(t--){
         int n;
         cin >> n;
+
         vector<long long> a(n);
-        for (int i = 0; i < n; ++i) cin >> a[i];
+        for(int i = 0; i < n; ++i){
+            cin >> a[i];
+        }
 
         int idx = -1;
         for (int i = 0; i < n; ++i) {
