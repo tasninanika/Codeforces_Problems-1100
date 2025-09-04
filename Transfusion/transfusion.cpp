3540fc24006628795f980ff519d9c0cfee51dca9
anikatasnin <jarintasnin09@gmail.com>
2025-09-04 20:05:01 +0600

    added Transfusion

--- Transfusion/transfusion.cpp
+++ Transfusion/transfusion.cpp
@@ -1,33 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<long long> a(n);
+    long long total_sum = 0;
+    long long odd_sum = 0;
+    long long even_sum = 0;
+    int odd_count = 0;
+    int even_count = 0;
+
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+
+        total_sum += a[i];
+        if((i + 1) % 2 != 0){
+            odd_sum += a[i];
+            odd_count++;
+        }
+        else{
+            even_sum += a[i];
+            even_count++;
+        }
+    }
+
+    if(total_sum % n != 0){
+        cout << "NO" << endl;
+        return;
+    }
+
+    long long avg = total_sum / n;
+
+    if(odd_sum % odd_count == 0 && even_sum % even_count == 0){
+        if((odd_sum / odd_count) == avg && (even_sum / even_count) == avg){
+            cout << "YES" << endl;
+            return;
+        }
+    }
+
+    cout << "NO" << endl;
+}
+
 int main(){
     int t;
     cin >> t;
 
     while(t--){
-        int n;
-        cin >> n;
-
-        vector<long long> a(n);
-        for(int i = 0; i < n; i++){
-            cin >> a[i];
-        }
-
-        bool ok = true;
-        for(int i = 1; i < n-1; i++){
-            if(a[i] != a[1]){
-                ok = false;
-                break;
-            }
-        }
-
-        if(ok){
-            cout << "YES" << endl;
-        }
-        else{
-            cout << "NO" << endl;
-        }
+        solve();
     }
     return 0;
 }
