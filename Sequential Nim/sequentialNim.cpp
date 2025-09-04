c657c961b867b97369912b4b2395bcde0514be12
anikatasnin <jarintasnin09@gmail.com>
2025-09-04 19:50:47 +0600

    added Sequential Nim

--- Sequential Nim/sequentialNim.cpp
+++ Sequential Nim/sequentialNim.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+
+        int idx = -1;
+        for (int i = 0; i < n; ++i) {
+            if (a[i] > 1) { idx = i; break; }
+        }
+
+        if (idx == -1) {
+            if (n % 2 == 1) cout << "First\n";
+            else cout << "Second\n";
+        }
+        else {
+            if ((idx + 1) % 2 == 1) cout << "First\n";
+            else cout << "Second\n";
+        }
+    }
+    return 0;
+}
 
