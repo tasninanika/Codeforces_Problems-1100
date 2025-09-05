3c834b43897d6f71391906014d581f554c461b49
anikatasnin <jarintasnin09@gmail.com>
2025-09-05 16:29:54 +0600

    added Line

--- Line/line.cpp
+++ Line/line.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        vector<ll> diffs;
+        ll sum = 0;
+
+        for (ll i = 0; i < n; i++) {
+            ll leftVal  = i;
+            ll rightVal = n - i - 1;
+            ll cur = (s[i] == 'L' ? leftVal : rightVal);
+            ll best = max(leftVal, rightVal);
+            sum += cur;
+            diffs.push_back(best - cur);
+        }
+
+        sort(diffs.rbegin(), diffs.rend());
+
+        for (ll i = 0; i < n; i++) {
+            sum += diffs[i];
+            cout << sum << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
 
