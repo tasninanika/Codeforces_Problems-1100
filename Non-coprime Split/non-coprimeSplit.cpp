c2430b37056c1279c57653f75e86afe93c1bc58a
anikatasnin <jarintasnin09@gmail.com>
2025-09-05 16:01:52 +0600

    added Non-coprime Split

--- Non-coprime Split/non-coprimeSplit.cpp
+++ Non-coprime Split/non-coprimeSplit.cpp
@@ -1,48 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     int t;
     cin >> t;
-
-    while(t--){
-        long long l, r;
+    while (t--) {
+        long l, r;
         cin >> l >> r;
 
-        if(r < 4){
-            cout << -1 << endl;
-            continue;
+        if (r < 4) {
+            cout << -1 << "\n";
         }
-
-        long long target_sum;
-        if(r % 2 == 0){
-            target_sum = r;
-        }
-        else{
-            target_sum = r - 1;
-        }
-
-        if(target_sum >= l){
-            cout << target_sum / 2 << " " << target_sum / 2 << endl;
-        }
-        else{
-            bool found = false;
-
-            for{long long p = 2; p * p <= l; ++p){
-                if(l % p == 0){
-                    cout << p << " " << l - p << endl;
-                    found = true;
+        else if (l == r) {
+            long div = -1;
+            for (long p = 2; p * p <= r; p++) {
+                if (r % p == 0) {
+                    div = p;
                     break;
                 }
             }
-            if(!found){
-                cout << -1 << endl;
-            }
+            if (div != -1) cout << div << " " << r - div << "\n";
+            else cout << -1 << "\n";
+        }
+        else {
+            cout << 2 << " " << (r % 2 == 0 ? r - 2 : r - 3) << "\n";
         }
     }
     return 0;
 }
-
