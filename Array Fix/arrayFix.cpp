65f2e0a222e3db3d2ecb1f33ba4a6197d0c2b90c
anikatasnin <jarintasnin09@gmail.com>
2025-09-07 18:48:51 +0600

    added Array Fix

--- Array Fix/arrayFix.cpp
+++ Array Fix/arrayFix.cpp
@@ -1,37 +1,49 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 void solve() {
     int n;
     cin >> n;
     vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
 
-    int max1 = 1e10;
-
-    for (int i = n - 1; i >= 0; i--) {
-        if (a[i] <= max1) {
-            max1 = min(max1, a[i]);
-        }
-        else {
-            vector<int> digits;
-            int x = a[i];
-
-            while (x > 0) {
-                digits.push_back(x % 10);
-                x /= 10;
-            }
-            for (int j = digits.size() - 1; j >= 0; j--) {
-                if (digits[j] > max1) {
-                    cout << "NO" << endl;
-                    return;
+    bool possible = true;
+    for (int i = n - 2; i >= 0; --i) {
+        if (a[i] > a[i+1]) {
+            if (a[i] >= 10) {
+                int d1 = a[i] / 10;
+                int d2 = a[i] % 10;
+                if (d1 > d2 || d2 > a[i+1]) {
+                    possible = false;
+                    break;
                 }
-                max1 = digits[j];
+                a[i] = d1;
+                a.insert(a.begin() + i + 1, d2);
+            }
+            else {
+                possible = false;
+                break;
             }
         }
     }
 
-    cout << "YES" << endl;
+    if (possible) {
+        for (int i = 0; i < a.size() - 1; ++i) {
+            if (a[i] > a[i+1]) {
+                possible = false;
+                break;
+            }
+        }
+    }
+
+    if (possible) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
 }
 
 int main() {
